2e26c905077425d8646479613b8ba208fd7b992a
khja240624 <khja240624@gmail.com>
2024-09-30 14:12:51 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/edu/kh/semi/SemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.semi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
